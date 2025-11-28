f0587064edd8ebc29a0c1084332c44e6e9efd462
Elixir239 <lancecedricdelatorre@gmail.com>
2025-11-28 09:10:08 +0800

    Add InvalidInput.java

---
+++ InvalidInput.java
@@ -0,0 +1,8 @@
+public class InvalidInput extends Exception {
+    public InvalidInput(String message) {
+        super();
+    }
+}
+    public class InvalidInput(String message) {
+        super(message);
+    }
